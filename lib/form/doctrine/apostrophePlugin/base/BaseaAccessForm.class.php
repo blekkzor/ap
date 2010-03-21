eadad3c35a5074d031b7d3e8330ac67af76cb7d4
tboutell <tboutell@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2010-03-21 22:55:56 +0000

    sample databases.yml is now UTF8-ready autogenerated stuff

--- lib/form/doctrine/apostrophePlugin/base/BaseaAccessForm.class.php
+++ lib/form/doctrine/apostrophePlugin/base/BaseaAccessForm.class.php
@@ -5,7 +5,7 @@
  *
  * @method aAccess getObject() Returns the current form's model object
  *
- * @package    asandbox
+ * @package    content
  * @subpackage form
  * @author     Your name here
  * @version    SVN: $Id: sfDoctrineFormGeneratedTemplate.php 24171 2009-11-19 16:37:50Z Kris.Wallsmith $
--- lib/form/doctrine/apostrophePlugin/base/BaseaMediaCategoryForm.class.php
+++ lib/form/doctrine/apostrophePlugin/base/BaseaMediaCategoryForm.class.php
@@ -5,7 +5,7 @@
  *
  * @method aMediaCategory getObject() Returns the current form's model object
  *
- * @package    asandbox
+ * @package    content
  * @subpackage form
  * @author     Your name here
  * @version    SVN: $Id: sfDoctrineFormGeneratedTemplate.php 24171 2009-11-19 16:37:50Z Kris.Wallsmith $
@@ -37,7 +37,10 @@ abstract class BaseaMediaCategoryForm extends BaseFormDoctrine
     ));
 
     $this->validatorSchema->setPostValidator(
-      new sfValidatorDoctrineUnique(array('model' => 'aMediaCategory', 'column' => array('name')))
+      new sfValidatorAnd(array(
+        new sfValidatorDoctrineUnique(array('model' => 'aMediaCategory', 'column' => array('name'))),
+        new sfValidatorDoctrineUnique(array('model' => 'aMediaCategory', 'column' => array('slug'))),
+      ))
     );
 
     $this->widgetSchema->setNameFormat('a_media_category[%s]');
