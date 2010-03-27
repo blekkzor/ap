11f94e8863440e31f1d851bb7edfc5b34d09767d
tboutell <tboutell@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2010-03-27 22:38:10 +0000

    Beginnings of functional tests for the a module. Database settings for test env. Notes toward more user fixtures for more interesting demos of user permissions, which we're very strong with and ought to demonstrate more.

--- test/bootstrap/functional.php
+++ test/bootstrap/functional.php
@@ -19,8 +19,19 @@ if (!isset($app))
 }
 
 require_once dirname(__FILE__).'/../../config/ProjectConfiguration.class.php';
+
+// This has a chicken and egg problem if the route to your home page is a CMS route and your test database doesn't exist yet. You can
+// resolve that with ./symfony doctrine:build-sql --env=test; ./symfony doctrine:insert-sql --env=test; ./symfony doctrine:data-load --env=test
+
 $configuration = ProjectConfiguration::getApplicationConfiguration($app, 'test', isset($debug) ? $debug : true);
 sfContext::createInstance($configuration);
 
 // remove all cache
 sfToolkit::clearDirectory(sfConfig::get('sf_app_cache_dir'));
+
+// DON'T do this. It doesn't know about fixtures,
+// and it's much slower than our own loadData() which we call from
+// the test constructor. Ours is faster because it reuses a cached
+// mysqldump when possible. TBB
+
+// Doctrine::loadData(sfConfig::get('sf_data_dir').'/fixtures');
